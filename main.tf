7e934d40ff11d0899609bcb55e0b1d2faac9eb2f
Derrin Chong <derrinchong@Derrins-Mac-Studio.local>
2024-09-08 20:00:49 -1000

    Add ability for terraform-sa to assume role for cloud build

--- main.tf
+++ main.tf
@@ -59,6 +59,16 @@ resource "google_project_iam_binding" "cloud_build_artifact_registry_permissions
   ]
 }
 
+# IAM binding for terraform-sa to act as the compute service account for Cloud Run
+resource "google_service_account_iam_binding" "cloud_run_act_as" {
+  service_account_id = var.compute_service_account_id
+  role               = "roles/iam.serviceAccountUser"
+
+  members = [
+    "serviceAccount:${var.terraform_sa_email}"
+  ]
+}
+
 # IAM binding for terraform-sa to deploy to Cloud Run
 resource "google_project_iam_binding" "cloud_build_run_permissions" {
   project = var.project_id
--- variables.tf
+++ variables.tf
@@ -22,3 +22,8 @@ variable "terraform_sa_email" {
   type        = string
   description = "The email of the service account to be used by Cloud Build."
 }
+
+variable "compute_service_account_id" {
+  type        = string
+  description = "The service account ID for Compute Engine or Cloud Run to use."
+}
