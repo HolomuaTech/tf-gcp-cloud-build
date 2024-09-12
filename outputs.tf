f1329feccefb6af728459b20f002d06301cc6038
Derrin Chong <derrinchong@Derrins-Mac-Studio.local>
2024-09-11 18:32:22 -1000

    Remove reference to trigger in outputs

--- outputs.tf
+++ outputs.tf
@@ -1,7 +1,3 @@
-output "cloud_build_trigger_name" {
-  value = google_cloudbuild_trigger.github_trigger.name
-}
-
 output "cloud_build_service_account" {
   value = "serviceAccount:${var.project_id}@cloudbuild.gserviceaccount.com"
 }
