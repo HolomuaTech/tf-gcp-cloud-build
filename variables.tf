8dfdbccfbe98af9090e42bc104c6893367f58074
Derrin Chong <derrinchong@Derrins-Mac-Studio.local>
2024-11-15 14:32:07 -1000

    Remove unused variables for github_owner and github_repo. they are not necessary for building a cloud build instance

--- variables.tf
+++ variables.tf
@@ -8,16 +8,6 @@ variable "region" {
   description = "The GCP region."
 }
 
-variable "github_owner" {
-  type        = string
-  description = "GitHub repository owner (username or organization)."
-}
-
-variable "github_repo" {
-  type        = string
-  description = "The name of the GitHub repository."
-}
-
 variable "terraform_sa_email" {
   type        = string
   description = "The email of the service account to be used by Cloud Build."
